8539cd593cff9aa12af3c0f35ef5cc18e9103f8a
Idhamm15 <idhamtamvanz123@gmail.com>
2023-05-31 10:30:37 +0700

    Serius Communication

---
+++ src/app/Koneksi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+/**
+ *
+ * @author idham
+ */
+public class Koneksi {
+    static connection KonnekKeDB {
+    try {
+        MysqlDataSource mds = new MysqlDataSource();
+        mds.setUser("root");
+        mds.setPassword("");
+        
+
+
+    } catch (Exception e) {
+    }
+}
+    
+}
